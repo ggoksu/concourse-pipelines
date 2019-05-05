0f8c38514c590867f63fa1894ed155f4c1656656
Gokhan Goksu <ggoksu@gmail.com>
2019-05-05 22:21:58 +0300

    msg

--- build/build-image.sh
+++ build/build-image.sh
@@ -6,8 +6,8 @@ env
 source /opt/resource/common.sh
 sanitize_cgroups
 start_docker \
-	"$insecure_registries" \
-  "$registry_mirror"
+	"" \
+  ""
 
 pack set-default-builder $BUILDER
 pack build $REPOSITORY/$APP:$TAG -p $APP
