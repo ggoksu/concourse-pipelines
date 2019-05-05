89fe2bda0b67b75a4e50ef03236e4853d2c29f3f
Gokhan Goksu <ggoksu@gmail.com>
2019-05-05 21:11:09 +0300

    updated

--- build/build-image.sh
+++ build/build-image.sh
@@ -4,6 +4,14 @@ pack version
 docker
 env
 source /opt/resource/common.sh
-start_docker 3 3 "" ""
+
+certs_to_file "$ca_certs"
+set_client_certs "$client_certs"
+sanitize_cgroups
+start_docker \
+	"" \
+  ""
+#log_in "$username" "$password" "$registry"
+
 pack set-default-builder $BUILDER
 pack build $REPOSITORY/$APP:$TAG -p $APP
