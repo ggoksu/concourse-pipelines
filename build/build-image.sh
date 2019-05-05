d936ef96c62ab6250c132e483fe640f399747c97
Gokhan Goksu <ggoksu@gmail.com>
2019-05-05 22:18:19 +0300

    msg

--- build/build-image.sh
+++ build/build-image.sh
@@ -3,6 +3,11 @@ set -x -e -u
 pack version
 docker
 env
-entrypoint.sh
+source /opt/resource/common.sh
+sanitize_cgroups
+start_docker \
+	"$insecure_registries" \
+  "$registry_mirror"
+
 pack set-default-builder $BUILDER
 pack build $REPOSITORY/$APP:$TAG -p $APP
