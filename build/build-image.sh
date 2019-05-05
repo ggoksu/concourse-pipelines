fb653297cfad1bb1cb986af4fefae1ed5ea0726b
Gokhan Goksu <ggoksu@gmail.com>
2019-05-05 21:34:37 +0300

    msg

--- build/build-image.sh
+++ build/build-image.sh
@@ -3,9 +3,5 @@ set -x -e -u
 pack version
 docker
 env
-source /opt/resource/common.sh
-
-docker run --privileged -d docker:dind
-
 pack set-default-builder $BUILDER
 pack build $REPOSITORY/$APP:$TAG -p $APP
