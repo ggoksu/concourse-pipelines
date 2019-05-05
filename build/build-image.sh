d9595dac40215ddde9b2d6b4e45044d3df82a8df
Gokhan Goksu <ggoksu@gmail.com>
2019-05-05 21:19:20 +0300

    msg

--- build/build-image.sh
+++ build/build-image.sh
@@ -5,13 +5,7 @@ docker
 env
 source /opt/resource/common.sh
 
-#certs_to_file "$ca_certs"
-#set_client_certs "$client_certs"
-sanitize_cgroups
-start_docker \
-	"" \
-  ""
-#log_in "$username" "$password" "$registry"
+docker run --privileged -d docker:dind
 
 pack set-default-builder $BUILDER
 pack build $REPOSITORY/$APP:$TAG -p $APP
