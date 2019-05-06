d86078e75a9a6e7a68692eb3d332a47b72811c9b
Gokhan Goksu <ggoksu@gmail.com>
2019-05-06 17:26:27 +0300

    msg

--- build/build-image.sh
+++ build/build-image.sh
@@ -8,8 +8,8 @@ sanitize_cgroups
 start_docker \
 	"$REPOSITORY" \
   ""
-log_in "$HARBOR_USER" "$HARBOR_PASS" "$REPOSITORY"
+#log_in "$HARBOR_USER" "$HARBOR_PASS" "$REPOSITORY"
 pack set-default-builder $BUILDER
 pack build $REPOSITORY/$PROJECT/$APP:$TAG -p $APP
-docker push $REPOSITORY/$PROJECT/$APP:$TAG
-#docker save -o image/image.tar $REPOSITORY/$APP:$TAG
+#docker push $REPOSITORY/$PROJECT/$APP:$TAG
+docker save -o image/image.tar $REPOSITORY/$APP:$TAG
