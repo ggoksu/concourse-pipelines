998d2c9d10827f9e1faf731d46895163ffa0e199
Gokhan Goksu <ggoksu@gmail.com>
2020-01-02 14:17:09 +0300

    cd to source corrected

--- build/build-image.sh
+++ build/build-image.sh
@@ -9,9 +9,9 @@ start_docker \
 	"$REGISTRY" \
   ""
 #log_in "$HARBOR_USER" "$HARBOR_PASS" "$REPOSITORY"
-cd apps/java-maven
+
 pack set-default-builder $BUILDER
-pack build $REGISTRY/$PROJECT/$APP:$TAG -p $APP
+pack build $REGISTRY/$PROJECT/$APP:$TAG -p $APP/apps/java-maven
 #docker push $REPOSITORY/$PROJECT/$APP:$TAG
 echo "$TAG" > image/tag
 docker save -o image/image.tar $REGISTRY/$PROJECT/$APP:$TAG
