e7dc473c7037e72f54dd1126a074daa3c844b007
Gokhan Goksu <ggoksu@gmail.com>
2020-01-02 15:38:44 +0300

    added own repo

--- build/build-image.sh
+++ build/build-image.sh
@@ -12,7 +12,7 @@ start_docker \
 echo 'LS'
 ls $APP
 pack set-default-builder $BUILDER
-pack build $REGISTRY/$PROJECT/$APP:$TAG -p $APP/apps/java-maven
+pack build $REGISTRY/$PROJECT/$APP:$TAG -p $APP
 #docker push $REPOSITORY/$PROJECT/$APP:$TAG
 echo "$TAG" > image/tag
 docker save -o image/image.tar $REGISTRY/$PROJECT/$APP:$TAG
